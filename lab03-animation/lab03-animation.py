5273e282c303013eef96f0525e5b6bf1127a8f58
MiguelM <miguel.martinezrodri@edu.uah.es>
2023-02-23 16:40:02 +0100

    Primer Dibujo

--- lab03-animation/lab03-animation.py
+++ lab03-animation/lab03-animation.py
@@ -5,12 +5,21 @@ def dibujar_suelo():
     arcade.draw_lrtb_rectangle_filled(0, 1700, 100, 0, arcade.color.GRAY)
 
 
+def dibujar_luna(altura):
+    arcade.draw_circle_filled(750, altura, 200, arcade.color.GRAY)
+    arcade.draw_circle_filled(870, altura, 160, arcade.color.DARK_BLUE)
+
+
 def dibujar_nube(posicion):
     arcade.draw_circle_filled(posicion + 150, 650, 80, arcade.color.WHITE)
     arcade.draw_circle_filled(posicion + 250, 630, 80, arcade.color.WHITE)
     arcade.draw_circle_filled(posicion + 350, 660, 80, arcade.color.WHITE)
     arcade.draw_circle_filled(posicion + 410, 660, 40, arcade.color.WHITE)
     arcade.draw_circle_filled(posicion + 75, 660, 40, arcade.color.WHITE)
+
+
+def dibujar_sol(altura):
+    arcade.draw_circle_filled(750, altura, 200, arcade.color.ORIOLES_ORANGE)
 
 
 def dibujar_persona(posicion, altura):
@@ -28,19 +37,30 @@ def dibujar_persona(posicion, altura):
 
 def dibujar(tiempo):
     arcade.start_render()
-
+    dibujar_luna(dibujar.luna_y)
+    dibujar_sol(dibujar.sol_y)
     dibujar_suelo()
-    dibujar_persona(dibujar.persona2_x, dibujar.persona2_y)
+    dibujar_persona(dibujar.persona2_x, 240)
     dibujar_persona(dibujar.persona1_x, dibujar.persona1_y)
+    dibujar_nube(dibujar.nube1_x)
+    dibujar_nube(dibujar.nube2_x)
     dibujar.persona1_x += 1
     dibujar.persona2_x += 1.3
-    dibujar.persona2_y -= 0.3
+    dibujar.nube1_x -= 0.9
+    dibujar.nube2_x -= 1.2
+    dibujar.sol_y -= 1.15
+    dibujar.luna_y += 0.9
 
 
+dibujar.sol_y = 360
+dibujar.nube2_x = 1200
 dibujar.persona1_x = 150
 dibujar.persona1_y = 300
 dibujar.persona2_x = 100
 dibujar.persona2_y = 700
+dibujar.nube1_x = 600
+dibujar.luna_y = -450
+
 
 def ejecutar():
     arcade.open_window(1500, 700, "ANIMACION")
